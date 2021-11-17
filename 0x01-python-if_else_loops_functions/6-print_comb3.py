9091fa8b2e1af19faabdeb97911b32b14fb4ce5f
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-11-16 16:27:58 -0800

    adding 6-print_comb3.py file

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,7 @@
 
 for no in range(10):
     for no2 in range(no+1, 10):
-        print("{:d}{:d}".format(no, no2), end=", ")
+        if no2 > 1:
+            print(", ", end="")
+        print("{:d}{:d}".format(no, no2), end="")
 print("\n")
