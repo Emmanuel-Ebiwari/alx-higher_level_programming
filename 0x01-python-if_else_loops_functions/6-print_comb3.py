9a295bc31807b6fe83f5591dc6f74ffbf54d897b
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-11-17 15:10:24 -0800

    minor fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 for no in range(10):
     for no2 in range(no+1, 10):
         if no2 > 1:
             print(", ", end="")
         print("{:d}{:d}".format(no, no2), end="")
-print("\n")
+print("")
