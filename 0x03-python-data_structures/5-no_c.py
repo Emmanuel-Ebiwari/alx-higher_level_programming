31be6d6c535c58e5184b23d23720432648f645aa
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-11-22 14:32:30 -0800

    minor fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,4 +2,4 @@
 def no_c(my_string):
     for c in my_string:
         if c == 'c' or c == 'C':
-           return my_string.translate({ord(c): None})
+            return my_string.translate({ord(c): None})
