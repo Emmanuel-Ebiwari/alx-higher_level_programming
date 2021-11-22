8ac81c6b4c2be6f41881c8a2baf0062e7b850ec2
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-11-22 15:43:53 -0800

    adding 8-multiple_returns.py file

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        return None
+        return (0, None)
     output = (len(sentence), sentence[0])
     return output
