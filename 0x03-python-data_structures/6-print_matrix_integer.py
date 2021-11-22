bcea2fe36e36013d739b0e1bee6f4b459ab851c1
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-11-22 14:58:03 -0800

    adding 6-print_matrix_integer.py file

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print("")
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}{}{}"
+                  .format(matrix[i][j], " " if j < 2 else "", "" 
+                          if j < 2 else "\n"), end="")
