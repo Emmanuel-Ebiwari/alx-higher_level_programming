670ffca7cd7abcfab747b9d774143c791c4a32fe
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-11-23 08:22:29 -0800

    minor fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,7 +7,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_a = (0, 0)
         else:
             tuple_a = (tuple_a[0], 0)
-    elif len2 < 2:
+    if len2 < 2:
         if len2 == 0:
             tuple_b = (0, 0)
         else:
