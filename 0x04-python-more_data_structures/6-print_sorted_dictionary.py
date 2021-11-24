dfabcb35f57c36304ef9439e85d542d3ebc9ac42
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-11-24 12:48:13 -0800

    adding 6-print_sorted_dictionary.py file

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     a_dict_items = a_dictionary.items()
     for key, value in sorted(a_dict_items, key=lambda d_items: d_items[0]):
-        print("{} : {}".format(key, value))
+        print("{}: {}".format(key, value))
