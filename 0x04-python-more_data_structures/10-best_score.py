b38bf2408ee17b52aadf92f2b950eb482d597ae2
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-11-26 04:16:48 -0800

    minor fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,8 @@ def best_score(a_dictionary):
         return None
     first_key = list(a_dictionary.keys())[0]
     max_val = a_dictionary[first_key]
+    ret = ""
     for key, val in a_dictionary.items():
         if val > max_val:
-            first_key = key
-    return first_key
+            ret = key
+    return ret
