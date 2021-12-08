8835f2f2e614eb40ca197a7afedc41d6d6c347f9
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-12-08 13:59:32 -0800

    adding files

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class."""
+
+
+class Square:
+    """Creates a square blueprint."""
+
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length of each sides of the square
+        """
+        self.__size = size
+©
