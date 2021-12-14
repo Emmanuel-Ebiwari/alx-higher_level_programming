60ac97614383da2c94e0f77e57addc638029feda
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-12-14 17:01:02 +0100

    adding 2-rectangle.py file

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -10,46 +10,46 @@ class Rectangle:
             height: the height of the rectangle
         """
 
-        self.width = width
-        self.height = height
+        self._width = width
+        self._height = height
+    @property
+    def width(self):
+        """getter method"""
+        return self.__width
 
-        @property
-        def width(self):
-            """getter method"""
-            return self.__width
+    @width.setter
+    def width(self, value):
+        """setter method"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        @width.setter
-        def width(self, value):
-            """setter method"""
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+    @property
+    def height(self):
+        """getter method"""
+        return self.__height
 
-        @property
-        def height(self):
-            """getter method"""
-            return self.__height
+    @height.setter
+    def height(self, value):
+        """setter method"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
-        @height.setter
-        def height(self, value):
-            """setter method"""
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
-        def area(self):
-             """A method that calculates the
-                area of a rectangle
-             """
-             return self.__width * self.__height
+    def area(self):
+        """A method that calculates the
+            area of a rectangle
+        """
+        return self.__width * self.__height
 
-        def perimeter(self):
-             """A method that calculates the
-                perimeter of a rectangle
-             """
-             if self.__width == 0 or self.__height == 0:
-                 return 0
-             return 2 * (self.__width + self.__height)
+    def perimeter(self):
+        """A method that calculates the
+            perimeter of a rectangle
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
