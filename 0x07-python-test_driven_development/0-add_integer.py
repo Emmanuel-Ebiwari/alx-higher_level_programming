e155f0e9d0ff25383230ef0a153f40b81e74269e
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-12-14 16:38:25 -0800

    adding files

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """A function that performs an addition
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        returns the sum of a and b
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError('a must be an integer')
+    elif not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError('b must be an integer')
+    a = int(a)
+    b = int(b)
+    return a + b
