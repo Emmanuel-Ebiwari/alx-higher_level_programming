433488a26c48fb375b8698c01b092094a3f13087
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-12-17 06:50:44 -0800

    adding 6-max_integer_test.py file

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,7 +7,7 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def test_case(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([3, 2, 4, 1]), 4)
+        self.assertEqual(max_integer([4, 2, 3, 1]), 4)
         self.assertEqual(max_integer([1]), 1)
         self.assertEqual(max_integer([-3, -2, -4, -1]), -1)
         self.assertEqual(max_integer([1000000, 2000000, 4000000, 1000000]), 4000000)
