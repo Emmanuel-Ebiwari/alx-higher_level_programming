e22bd6ea756cc4f1f05b77875fc3228fa4bd6c28
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-12-17 06:57:29 -0800

    adding 6-max_integer_test.py file

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,6 +13,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1000000, 2000000, 4000000, 1000000]), 4000000)
         self.assertEqual(max_integer([1, 2, 3, 4, 4.5]), 4.5)
         self.assertEqual(max_integer((1, 2, 3, 4)), 4)
+        self.assertEqual(max_integer([]), None)
 
     def test_types(self):
         self.assertRaises(TypeError, max_integer, [3, 2, 4, '1'])
