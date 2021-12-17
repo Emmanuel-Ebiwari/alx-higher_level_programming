f79997a60e279728ba4cf27fa74e740157deaf51
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-12-17 03:20:09 -0800

    adding 4-print_square.py file

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" a function that prints a square """
+
+
+def print_square(size):
+    """Print a square with character #
+        Args:
+            size: size of the square
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        print('#' * size)
