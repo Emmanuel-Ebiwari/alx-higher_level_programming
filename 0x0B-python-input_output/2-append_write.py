40907160679dbf0d971351a7e2ab45de418a8b16
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-12-22 09:13:21 -0800

    adding 2-append_write.py file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Creates a function that appends a string to a text file
+    (UTF8) and returns the number of characters written
+"""
+
+
+def append_write(filename="", text=""):
+    """Appends to a file and returns the number of characters written"""
+    with open(filename, mode="a", encoding="utf-8") as myFile:
+        myFile.write(text)
+    with open(filename, encoding="utf-8") as myFile:
+        return len(myFile.read())
