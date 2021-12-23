7c42a0ee89ee20ad42c545bfc721e83eda4f3f2a
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2021-12-23 06:39:55 -0800

    minor fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,8 +22,8 @@ class Rectangle(Base):
         """setter method"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -36,8 +36,8 @@ class Rectangle(Base):
         """setter method"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
