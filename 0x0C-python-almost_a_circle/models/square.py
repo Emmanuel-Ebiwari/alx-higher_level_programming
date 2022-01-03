2349a6833d4dafc210c69071237f4fc6eb26ba2a
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2022-01-03 14:28:17 -0800

    adding models/square.py file

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Creates a Square class that inherits from Rectangle class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        self.__size = size
+
+    @property
+    def size(self):
+        """getter method"""
+        return super(Square, self).width
+
+    @size.setter
+    def size(self, value):
+        """setter method"""
+        Square.width.fset(self, value)
