3844526b21d2e069a86564f120f8c1bf1860a34a
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2022-01-03 16:16:08 -0800

    updating models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,3 +21,15 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+
+        filename = cls.__name__ + ".json"
+        list_ins = []
+        with open(filename, "w", encoding="utf-8") as myfile:
+            if list_objs:
+                for i in list_objs:
+                    list_ins.append(i.to_dictionary())
+            myfile.write(cls.to_json_string(list_ins))
