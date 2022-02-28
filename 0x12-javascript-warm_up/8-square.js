36d91bc412983eaeb901b140d7132e5baf9ab503
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2022-02-28 15:03:09 -0800

    adding 8-square.js file

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const { argv } = require('process');
+
+let x = 'X'
+if (isNaN(argv[2])) console.log('Missing size');
+for (let i = 0; i < argv[2]; i++) {
+  console.log(x.repeat(argv[2]));
+}
