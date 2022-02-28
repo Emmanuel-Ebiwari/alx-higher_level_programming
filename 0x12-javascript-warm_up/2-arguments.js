42b8741e2ce3ca83e838f5cbf81ca8bff7146c0b
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2022-02-28 09:43:49 -0800

    adding 2-arguments.js file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-let argLen = argv.length
-
-argLen > 2 ?  argLen < 4 ? console.log('Argument found') : console.log('Arguments found') : console.log('No argument');
+const argLen = argv.length;
+argLen > 2 ? argLen < 4 ? console.log('Argument found') : console.log('Arguments found') : console.log('No argument');
