602c55fb874a57a9483547f96d22b45ce956b5f7
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2022-02-28 14:32:16 -0800

    adding 7-multi_c.js file

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const { argv } = require('process');
+
+if (isNaN(argv[2])) console.log('Missing number of occurrences');
+for (let i = 0; i < argv[2]; i++) {
+  console.log('C is fun');
+}
