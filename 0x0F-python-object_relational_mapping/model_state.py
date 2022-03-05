74a1cd37ae80e77d96e2edb4dfc03ea572b4d97f
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2022-03-05 15:51:03 -0800

    adding model_state.py file

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,6 +13,7 @@ Base = declarative_base()
 
 class States(Base):
     """Objects that represents the state table in the database"""
+
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
