964a004ea37c69d65dae9491696cecaedb74ee3c
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2022-03-05 14:45:21 -0800

    adding 4-cities_by_state.py file

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+ lists all cities from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db_con = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                             passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db_con.cursor()
+    sql_query = "SELECT id, name FROM cities ORDER BY id ASC"
+    cur.execute(sql_query)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db_con.close()
