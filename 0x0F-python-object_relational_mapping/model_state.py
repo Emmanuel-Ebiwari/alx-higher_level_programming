c45ed62c8caef8b98102f1f0fbfe1243ec0bbe8b
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2022-03-06 12:16:19 -0800

    adding model_state.py file

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,7 +4,6 @@ python file that contains the class definition of a State and
 an instance Base = declarative_base():
 """
 
-import "6-model_state.py"
 import sqlalchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
