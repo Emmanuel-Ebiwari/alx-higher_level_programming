20efc7de512cdc2e3cb724cc6caa70dbb4d490d1
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2022-06-16 03:57:31 -0700

    adding files

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-Module 0-hbtn-status
-Fetches https://intranet.hbtn.io/status
+A Python script that fetches
+https://alx-intranet.hbtn.io/status
 """
 
 
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         res = response.read()
         print('Body response:')
         print("\t- type: {}".format(type(res)))
